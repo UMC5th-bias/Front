506fb8337581809ecae4bd3bd0389528c7bdb939
Linaeformin <minsa030407@gmail.com>
2024-01-08 03:14:11 +0900

    스크롤 에러 수정

--- app/src/main/java/com/example/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/example/favoriteplace/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //fragment라 바로 실행이 불가능해서 확인하고자 넣은 코드. merge 시 삭제 요망
         val RallyCategoryFragment=RallyCategoryFragment()
         supportFragmentManager.beginTransaction().replace(R.id.frameLayout,RallyCategoryFragment).commit()
     }
