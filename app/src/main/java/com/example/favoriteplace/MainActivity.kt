7662fce7f247b5376d7ccefa02db20c8b00300e1
eunsxx <88652027+eunsxx@users.noreply.github.com>
2024-01-10 00:32:15 +0900

    [Feat] 성지순례 메인 화면(비회원) 레이아웃 구현

--- app/src/main/java/com/example/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/example/favoriteplace/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.favoriteplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_rally_home_nonmember)
+
+        val interestedRallyLayout: ConstraintLayout = findViewById(R.id.interestedRally_cl)
+        val certificatedRallyLayout: ConstraintLayout = findViewById(R.id.certification_board_cl)
+
+        interestedRallyLayout.setOnClickListener {
+            val inflater = layoutInflater
+            val layout: View = inflater.inflate(R.layout.custom_toast, findViewById(R.id.custom_toast_message))
+
+            val textView = layout.findViewById<TextView>(R.id.custom_toast_message)
+            textView.text = "로그인 후 이용 가능한 메뉴입니다."
+
+            with (Toast(applicationContext)) {
+                setDuration(Toast.LENGTH_SHORT)
+                setView(layout)
+                show()
+            }
+        }
+
+        certificatedRallyLayout.setOnClickListener {
+            val inflater = layoutInflater
+            val layout: View = inflater.inflate(R.layout.custom_toast, findViewById(R.id.custom_toast_message))
+
+            val textView = layout.findViewById<TextView>(R.id.custom_toast_message)
+            textView.text = "로그인 후 이용 가능한 메뉴입니다."
+
+            with (Toast(applicationContext)) {
+                setDuration(Toast.LENGTH_SHORT)
+                setView(layout)
+                show()
+            }
+        }
     }
+
+
+
 }
