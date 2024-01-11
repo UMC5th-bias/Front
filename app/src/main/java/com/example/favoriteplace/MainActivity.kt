7c6a03e1a27443533dbb328facc148a98b813fb2
Linaeformin <minsa030407@gmail.com>
2024-01-11 23:01:57 +0900

    마지막 수정

--- app/src/main/java/com/example/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/example/favoriteplace/MainActivity.kt
@@ -7,10 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //fragment라 바로 실행이 불가능해서 확인하고자 넣은 코드. merge 시 삭제 요망
-        val RallyCategoryFragment=RallyCategoryFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,RallyCategoryFragment).commit()
-        //
     }
 }
