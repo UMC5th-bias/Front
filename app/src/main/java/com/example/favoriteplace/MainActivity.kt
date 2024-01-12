c2597f75a0d09b0ef65b5da2f437de5b86dc0247
eunsxx <88652027+eunsxx@users.noreply.github.com>
2024-01-12 11:58:43 +0900

    [Style] color 변수명 변경

--- app/src/main/java/com/example/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/example/favoriteplace/MainActivity.kt
@@ -14,29 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_post_detail)
 
-        // 댓글 입력창의 참조를 얻습니다.
-        val commentEditText = findViewById<EditText>(R.id.comment_register_et)
-
-// 루트 뷰의 참조를 얻습니다.
-        val rootView = findViewById<View>(android.R.id.content)
-
-// 뷰 트리 옵저버를 사용하여 키보드의 상태를 감지합니다.
-        rootView.viewTreeObserver.addOnGlobalLayoutListener {
-            val rect = Rect()
-            rootView.getWindowVisibleDisplayFrame(rect)
-            val screenHeight = rootView.height
-
-            // 화면 높이와 가시 영역의 차이를 계산하여 키보드의 높이를 추정합니다.
-            val keypadHeight = screenHeight - rect.bottom
-
-            if (keypadHeight > screenHeight * 0.15) { // 키보드가 열려있음
-                commentEditText.setPadding(0, 0, 0, keypadHeight) // 키보드 높이만큼 패딩 추가
-            } else { // 키보드가 닫혀있음
-                commentEditText.setPadding(0, 0, 0, 0) // 패딩 제거
-            }
-        }
-
-
 //        setContentView(R.layout.fragment_rally_home_nonmember)
 //
 //        val interestedRallyLayout: ConstraintLayout = findViewById(R.id.interestedRally_cl)
