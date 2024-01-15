6742213964e75ea8d50faff597661d7bb8a52669
Seungman Lim <woori4158@gmail.com>
2024-01-15 14:03:56 +0900

    [Refactor] fragment 이름 수정
    fragment_communitymain.xml -> fragment_community_main.xml

--- app/src/main/java/com/example/favoriteplace/CommunityMainFragment.kt
+++ app/src/main/java/com/example/favoriteplace/CommunityMainFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.favoriteplace.databinding.FragmentCommunitymainBinding
+import com.example.favoriteplace.databinding.FragmentCommunityMainBinding
 
 class CommunityMainFragment: Fragment() {
 
-    lateinit var binding: FragmentCommunitymainBinding
+    lateinit var binding: FragmentCommunityMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCommunitymainBinding.inflate(inflater,container,false)
+        binding = FragmentCommunityMainBinding.inflate(inflater,container,false)
 
         return binding.root
     }
