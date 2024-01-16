4c3a3d993a23e1e71dc4e2cf70b9c0467efdbfbc
ha-es <dmstj00423@naver.com>
2024-01-16 10:26:52 +0900

    [REFACTOR] HomeFragment,FragmentHome, FragmentFindPwd 및 dialog 수정

--- app/src/main/java/com/example/favoriteplace/HomeFragment.kt
+++ app/src/main/java/com/example/favoriteplace/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
@@ -25,21 +26,6 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
 
-        // 실시간 인기글
-        homeItemDatas.apply {
-            add(HomeItem(R.drawable.home_realtime_img1,"여기짱재밋어요하하하하하하하","#날씨의아이","#도쿄","1시간 전","성지순례 인증"))
-            add(HomeItem(R.drawable.home_realtime_img2,"나고야 주변 성지순례","#날씨의아이","#너의이름은","1시간 전","자유게시판"))
-            add(HomeItem(R.drawable.home_realtime_img3,"날씨의아이어쩌구","#날씨의아이"," ","1시간 전","성지순례 인증"))
-            //add(HomeItem(R.drawable.home_realtime_img2,"토토로보고 왔어요오오~","#이웃집 토토로"," ","1시간 전","성지순례 인증"))
-        }
-
-
-        // 실시간 인기글 데이터 정보
-        val homeItemRVAdapter = HomeItemRVAdapter(homeItemDatas)
-        binding.recyclerView.adapter=homeItemRVAdapter
-        binding.recyclerView.layoutManager=LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL,false)
-
-
 
         //광고 배너
         val bannerAdapter = BannerVPAdapter(this)
@@ -51,7 +37,7 @@ class HomeFragment : Fragment() {
 
 
 
-        // 로그인 버튼
+        //로그인 버튼
         binding.homeLoginBtn.setOnClickListener {
             startActivity(Intent(activity,LoginActivity::class.java))
         }
@@ -61,8 +47,6 @@ class HomeFragment : Fragment() {
         return binding.root
 
     }
-
-
 
 
 
