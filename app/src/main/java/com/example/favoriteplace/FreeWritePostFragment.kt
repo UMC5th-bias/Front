0c9921785ad7d56db588bb758a0d6bd346989c61
eunsxx <88652027+eunsxx@users.noreply.github.com>
2024-01-18 20:37:55 +0900

    [Feat] 자유게시판 글쓰기 카메라 선택 기능 구현

--- app/src/main/java/com/example/favoriteplace/FreeWritePostFragment.kt
+++ app/src/main/java/com/example/favoriteplace/FreeWritePostFragment.kt
@@ -1,6 +1,9 @@
 package com.example.favoriteplace
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,6 +16,8 @@ import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.setMargins
 import androidx.fragment.app.Fragment
 import com.example.favoriteplace.databinding.FragmentFreeWritePostBinding
@@ -21,7 +26,10 @@ class FreeWritePostFragment : Fragment() {
     lateinit var binding: FragmentFreeWritePostBinding
 
     private val REQUEST_CODE_GALLERY = 100
+    private val REQUEST_IMAGE_CAPTURE = 1
     private val selectedImages = mutableListOf<Uri>()
+    private val selectedBitmaps = mutableListOf<Bitmap>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -35,6 +43,46 @@ class FreeWritePostFragment : Fragment() {
         binding.writePostGalleryIv.setOnClickListener {
             openGallery()
         }
+
+        binding.writePostCameraIv.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // 권한이 없다면, 권한 요청
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_IMAGE_CAPTURE
+                )
+            } else {
+                // 권한이 있다면, 카메라 앱 시작
+                dispatchTakePictureIntent()
+            }
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // 권한이 부여되면, 카메라 앱 시작
+                    dispatchTakePictureIntent()
+                }
+                return
+            }
+        }
     }
 
     private fun openGallery() {
@@ -47,7 +95,7 @@ class FreeWritePostFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_CODE_GALLERY && resultCode == android.app.Activity.RESULT_OK) {
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == Activity.RESULT_OK) {
             val clipData = data?.clipData
             if (clipData != null) {
                 // 여러 이미지 처리
@@ -61,6 +109,14 @@ class FreeWritePostFragment : Fragment() {
                 data?.data?.let { uri ->
                     addImageToLayout(uri)
                 }
+            }
+        }
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            // 결과 데이터에서 Bitmap 추출
+            val imageBitmap = data?.extras?.get("data") as? Bitmap
+            imageBitmap?.let {
+                addImageToLayout(it)
             }
         }
     }
@@ -103,8 +159,55 @@ class FreeWritePostFragment : Fragment() {
         binding.imageContainer.addView(frameLayout)
         selectedImages.add(uri)
     }
+
+    private fun addImageToLayout(bitmap: Bitmap) {
+        if (selectedBitmaps.size >= 5) return // 이미 5개의 이미지가 선택된 경우 추가하지 않음
+
+        val frameLayout = FrameLayout(requireContext()).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                marginEnd = 10
+            }
+        }
+
+        val imageView = ImageView(requireContext()).apply {
+            layoutParams = FrameLayout.LayoutParams(100, 100)
+            scaleType = ImageView.ScaleType.CENTER_CROP
+            setImageBitmap(bitmap) // Bitmap 설정
+        }
+
+        val deleteButton = ImageButton(requireContext()).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                50, // 버튼 크기 조정
+                50
+            ).apply {
+                gravity = Gravity.TOP or Gravity.END
+                setMargins(10)
+            }
+            setImageResource(R.drawable.ic_delete_btn) // 커스텀 이미지 사용
+            background = null // 배경을 투명하게 설정
+            setOnClickListener {
+                // removeImage
+                removeImageFromLayout(frameLayout, bitmap)
+            }
+        }
+
+        frameLayout.addView(imageView)
+        frameLayout.addView(deleteButton)
+        binding.imageContainer.addView(frameLayout)
+        selectedBitmaps.add(bitmap) // selectedBitmaps는 선택된 Bitmap 객체를 추적하는 리스트
+    }
+
     private fun removeImageFromLayout(frameLayout: FrameLayout, uri: Uri) {
         binding.imageContainer.removeView(frameLayout)
         selectedImages.remove(uri)
     }
+
+    private fun removeImageFromLayout(frameLayout: FrameLayout, bitmap: Bitmap) {
+        binding.imageContainer.removeView(frameLayout) // FrameLayout을 메인 레이아웃에서 제거
+        selectedBitmaps.remove(bitmap) // 선택된 Bitmap 리스트에서 해당 Bitmap 제거
+    }
+
 }
