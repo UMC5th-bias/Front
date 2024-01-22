cd4051f03e58fc1449ca440ee27c0264cf320e11
Seungman Lim <woori4158@gmail.com>
2024-01-22 16:14:00 +0900

    [Feat] 회원가입 약관동의 페이지 디자인

--- app/src/main/java/com/example/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/example/favoriteplace/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initBottomNavigation(){
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frameLayout, HomeFragment())
+            .replace(R.id.main_frameLayout, SignUpConditionConfirmFragment())
             .commitAllowingStateLoss()
 
 
