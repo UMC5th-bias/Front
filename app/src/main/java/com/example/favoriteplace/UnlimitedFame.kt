55d169a197807fb4f2407d2ab2ed902e4550eba2
Linaeformin <minsa030407@gmail.com>
2024-02-12 04:40:34 +0900

    [FEAT] 데이터 클래스에 id 값 추가

--- app/src/main/java/com/example/favoriteplace/UnlimitedFame.kt
+++ app/src/main/java/com/example/favoriteplace/UnlimitedFame.kt
@@ -2,5 +2,6 @@ package com.example.favoriteplace
 
 data class UnlimitedFame(
     var fameImg: String ="",
-    var cost: String?=""
+    var cost: String?="",
+    var id: Int=0
 )
--- app/src/main/java/com/example/favoriteplace/UnlimitedIcon.kt
+++ app/src/main/java/com/example/favoriteplace/UnlimitedIcon.kt
@@ -3,5 +3,6 @@ package com.example.favoriteplace
 data class UnlimitedIcon(
     var iconImg: String ="",
     var cost: String?="",
-    var title: String?=""
+    var title: String?="",
+    var id: Int=0
 )
