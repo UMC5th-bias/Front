7b1e0b07fa43f1a9c7cc61182205379d2a004ccd
Seungman Lim <woori4158@gmail.com>
2024-02-15 20:43:02 +0900

    [Feat] 지도에 현위치 표시 구현

--- app/src/main/java/com/example/favoriteplace/RallyPlaceFragment.kt
+++ app/src/main/java/com/example/favoriteplace/RallyPlaceFragment.kt
@@ -10,9 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.favoriteplace.databinding.FragmentRallyplaceBinding
 import com.google.firebase.annotations.concurrent.UiThread
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,7 +23,8 @@ import retrofit2.Response
 class RallyPlaceFragment: Fragment(), OnMapReadyCallback {
 
     lateinit var binding: FragmentRallyplaceBinding
-
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,14 +36,20 @@ class RallyPlaceFragment: Fragment(), OnMapReadyCallback {
 
         var regionList: List<Region> = emptyList()
 
+        //지도 fragment
         val fm = childFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.rally_map_mv) as MapFragment?
+        val mapFragment = fm.findFragmentById(R.id.rally_place_mv_container) as MapFragment?
             ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.rally_map_mv, it).commit()
+                fm.beginTransaction().add(R.id.rally_place_mv_container, it).commit()
             }
-
         mapFragment.getMapAsync(this)
 
+        // 위치 구하는 모듈
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+
+
+        // 불러온 지역에 해당하는 버튼 활성화 및 RecyclerView 설정
         fun setRegionRV() {
             //도쿄
             val tempTyoko = regionList.filter {it.state == "도쿄도" }
@@ -122,8 +131,30 @@ class RallyPlaceFragment: Fragment(), OnMapReadyCallback {
     }
 
 
+    //지도 준비됐을때 호출
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
-        // ...
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Face // 위치 추적 활성화
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true // 현위치 버튼 활성화
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    //권한 요청 코드
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
