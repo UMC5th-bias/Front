b5a2e31426d1db7b0e180e67e6bf850a42c20faf
Seungman Lim <woori4158@gmail.com>
2024-02-19 16:00:17 +0900

    [Feat] 마이페이지 내정보(메인) api연동

--- app/src/main/java/com/example/favoriteplace/MyService.kt
+++ app/src/main/java/com/example/favoriteplace/MyService.kt
@@ -3,11 +3,16 @@ package com.example.favoriteplace
 import retrofit2.Call
 import retrofit2.http.Header
 import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.GET
 
 interface MyService {
     @POST("/auth/logout")
     fun logout(
         @Header("Authorization") authorization: String?
     ): Call<String>
+
+    @GET("/my")
+    fun getMyInfo(
+        @Header("Authorization") authorization: String?
+    ): Call<MyInfo>
 }
